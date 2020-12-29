0dc2cf225bf1ea508822c9a70870123e95a55685
Dheeraj <dhiraj02gupta@gmail.com>
2020-12-29 20:12:54 +0530

    no change

---
+++ Bumble/app/src/main/java/com/nero/bumble/ModeToGetStartedActivity.java
@@ -0,0 +1,14 @@
+package com.nero.bumble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModeToGetStartedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode_to_get_started);
+    }
+}
