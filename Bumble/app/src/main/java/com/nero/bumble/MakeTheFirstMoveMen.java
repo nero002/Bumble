9d5a428ebd14b4e422f1cfda0cfd0de51adfe3c2
Dheeraj <dhiraj02gupta@gmail.com>
2020-12-30 12:14:06 +0530

    pulling the soubia work

--- Bumble/app/src/main/java/com/nero/bumble/MakeTheFirstMoveMen.java
+++ Bumble/app/src/main/java/com/nero/bumble/MakeTheFirstMoveMen.java
@@ -2,13 +2,21 @@ package com.nero.bumble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MakeTheFirstMoveMen extends AppCompatActivity {
+
+    private Button mbtn_GotIt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_the_first_move_men);
+
+        mbtn_GotIt = findViewById(R.id.btn_GotIt);
+        Intent intent = new Intent()
+
     }
 }
