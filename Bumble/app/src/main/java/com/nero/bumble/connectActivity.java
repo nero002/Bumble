a2d2cd2cc67782627f6c26aed708ce8cdc2a90be
soubiakulsum <soubiakulsum@gmail.com>
2020-12-30 17:16:45 +0530

    intent day

--- Bumble/app/src/main/java/com/nero/bumble/connectActivity.java
+++ Bumble/app/src/main/java/com/nero/bumble/connectActivity.java
@@ -2,13 +2,32 @@ package com.nero.bumble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class connectActivity extends AppCompatActivity {
+    private Button mBtngotitconnect;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+        initializeViewsAndListeners();
+        
+    }
+
+    private void initializeViewsAndListeners() {
+        mTvphonenumber = findViewById(R.id.mTvphonenumber);
+        mBtngotitconnect = findViewById(R.id.mBtngotitconnect);
+        mBtngotitconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(connectActivity.this,)
+            }
+        });
     }
 }
--- Bumble/app/src/main/java/com/nero/bumble/firstMoveActivity.java
+++ Bumble/app/src/main/java/com/nero/bumble/firstMoveActivity.java
@@ -2,13 +2,31 @@ package com.nero.bumble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class firstMoveActivity extends AppCompatActivity {
+    private Button mBtngotit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_move);
+        initializeViewsAndListeners();
+    }
+
+    private void initializeViewsAndListeners() {
+        mBtngotit = findViewById(R.id.mBtngotit);
+        mBtngotit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(firstMoveActivity.this,connectActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
