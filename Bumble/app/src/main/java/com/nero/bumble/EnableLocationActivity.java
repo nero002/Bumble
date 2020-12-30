f775fb4a6f9ee9c78875e5a828b29f64524854bb
soubiakulsum <soubiakulsum@gmail.com>
2020-12-30 11:51:10 +0530

    otp activity for java

--- Bumble/app/src/main/java/com/nero/bumble/EnableLocationActivity.java
+++ Bumble/app/src/main/java/com/nero/bumble/EnableLocationActivity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class FirstNameActivity extends AppCompatActivity {
-    
+public class EnableLocationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first_name);
+        setContentView(R.layout.activity_enable);
     }
 }
