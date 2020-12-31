9c022d6eb737c43429420b9b30b92eaee5c46720
Dheeraj <dhiraj02gupta@gmail.com>
2020-12-31 20:51:44 +0530

    Fb login page java+xml complte , bottom of men and women chat logo change , Filter activity

--- Bumble/app/src/main/java/com/nero/bumble/SwipeManActivity.java
+++ Bumble/app/src/main/java/com/nero/bumble/SwipeManActivity.java
@@ -1,14 +1,59 @@
 package com.nero.bumble;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SwipeManActivity extends AppCompatActivity {
+    private ImageButton miBtnFilter;
+    private ImageButton miBtnUser;
+    private ImageButton miBtnHeart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_man);
+        initializeViewsAndListeners();
+
+    }
+
+    private void initializeViewsAndListeners() {
+        miBtnFilter = findViewById(R.id.iBtnFilter);
+        miBtnUser = findViewById(R.id.iBtnUser);
+        miBtnHeart = findViewById(R.id.iBtnHeart);
+
+        //for filter button
+
+        miBtnFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SwipeManActivity.this, FilterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Profile visit
+
+        miBtnUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SwipeManActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Heart Button
+
+        miBtnHeart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SwipeManActivity.this, BeelineActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- Bumble/app/src/main/java/com/nero/bumble/SwipeWomenActivity.java
+++ Bumble/app/src/main/java/com/nero/bumble/SwipeWomenActivity.java
@@ -1,14 +1,53 @@
 package com.nero.bumble;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SwipeWomenActivity extends AppCompatActivity {
+    private ImageButton miBtnFilter;
+    private ImageButton miBtnUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_women);
+        initializeViewsAndListeners();
+    }
+
+    private void initializeViewsAndListeners() {
+        miBtnFilter = findViewById(R.id.iBtnFilter);
+        miBtnUser = findViewById(R.id.iBtnUser);
+        //for filter button
+
+        miBtnFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SwipeWomenActivity.this, FilterActivity.class);
+                startActivity(intent);
+            }
+        });
+        // Profile visit
+        miBtnUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SwipeWomenActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
