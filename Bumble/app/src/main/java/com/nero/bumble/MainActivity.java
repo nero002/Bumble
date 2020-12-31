5e06113de6e097d46e45fd175c8e9a02d77c159f
soubiakulsum <soubiakulsum@gmail.com>
2020-12-31 21:54:58 +0530

    2 activities

--- Bumble/app/src/main/java/com/nero/bumble/MainActivity.java
+++ Bumble/app/src/main/java/com/nero/bumble/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_swipe_man);
 
 //        new Handler().postDelayed(new Runnable() {
 //
