55398b6949c6c500fe5129cfc4e5b92684a31952
Ishwar Kumar <kumarishwar623@gmail.com>
2020-12-31 18:50:03 +0530

    Five Activities all done no changes required

--- Bumble/app/src/main/java/com/nero/bumble/SwipeWomenActivity.java
+++ Bumble/app/src/main/java/com/nero/bumble/SwipeWomenActivity.java
@@ -1,7 +1,9 @@
 package com.nero.bumble;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.os.Build;
 import android.os.Bundle;
 
 public class SwipeWomenActivity extends AppCompatActivity {
@@ -10,5 +12,13 @@ public class SwipeWomenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_women);
+        changeNavigationBarColour();
+
+    }
+
+    private void changeNavigationBarColour() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.black));
+        }
     }
 }
